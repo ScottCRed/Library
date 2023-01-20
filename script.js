35c4d2a1c49de4d16ed2929774eb49f48903010f
ScottCRed <ScottC@redpandasoftware.co.za>
2023-01-20 12:27:31 +0200

    Create info function to report back info

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.info = () => {return this.title + ' by '+ this.author + ', ' + this.pages + ' pages' + ', has been read'
+  }
 }
 
 const book1 = new Book('Hooray', 'Me', '300', true);
 
 console.log(book1);
+console.log(book1.info())
